63bb12773e9cbcfd7b1657c981d14bcc2eca5928
Designation6of10 <sixoften2@gmail.com>
2022-09-05 11:46:55 -0600

    Fixed bugs with exit placement

--- Homework 11/js/game.js
+++ Homework 11/js/game.js
@@ -1,17 +1,26 @@
 new p5();
-var horzCenter = (windowWidth / 2);
-var vertCenter = (windowHeight / 2);
+var canvasHeight = (windowHeight - 20);
+var canvasWidth = (windowWidth - 20);
+var horzCenter = (canvasWidth / 2);
+var vertCenter = (canvasHeight / 2);
 var backgroundColor = [0, 150, 0];
 var wallWidth = 7;
 var snap = '';
 var exitPlaced = false;
 var exitTextPos = [];
 
-var exitPos = [floor(random() * windowWidth), floor(random() * windowHeight)];
+var exitPos = [floor(random() * canvasWidth), floor(random() * canvasHeight)];
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(canvasWidth, canvasHeight);
 }
+
+function windowResized() {
+    canvasHeight = (windowHeight - 20);
+    canvasWidth = (windowWidth - 20);
+    resizeCanvas(canvasWidth,canvasHeight);
+    exitPlaced = false;
+  }
 
 function draw() {
     background(backgroundColor);
@@ -19,11 +28,12 @@ function draw() {
     createExit();
 }
 
+
 function createWalls() {
     stroke(0);
     strokeWeight(wallWidth);
     fill(backgroundColor);
-    rect(wallWidth / 2, wallWidth / 2, (windowWidth - wallWidth), (windowHeight - wallWidth));
+    rect(wallWidth / 2, wallWidth / 2, (canvasWidth - wallWidth), (canvasHeight - wallWidth));
 }
 
 function createExit() {
@@ -34,7 +44,14 @@ function createExit() {
     strokeWeight(0);
     fill(backgroundColor);
     square(exitPos[0] - (2 * wallWidth), exitPos[1] - (2 * wallWidth), wallWidth * 4)
-    signExit();
+    
+    if (exitPlaced == false) {
+        signExit();
+    }
+    
+    strokeWeight(0);
+    fill(0)
+    text('Exit',exitTextPos[0],exitTextPos[1]);
 }
 
 function snapExit() {
@@ -46,12 +63,12 @@ function snapExit() {
             snap = 't';
         //otherwise, snap to bottom
         } else {
-            exitPos[1] = (windowHeight + (wallWidth / 2))
+            exitPos[1] = (canvasHeight - (wallWidth / 2))
             snap = 'b';
         }
         //clear corner horizontal
-        if (exitPos[0] > (windowWidth - (4 * wallWidth))) {
-            exitPos[0] = (windowWidth - (4 * wallWidth));
+        if (exitPos[0] > (canvasWidth - (4 * wallWidth))) {
+            exitPos[0] = (canvasWidth - (4 * wallWidth));
         } else if (exitPos[0] < (4 * wallWidth)) {
             exitPos[0] = (4 * wallWidth);
         }
@@ -62,45 +79,40 @@ function snapExit() {
             snap = 'l';
         //otherwise, snap right
         } else {
-            exitPos[0] = (windowWidth + (wallWidth / 2))
+            exitPos[0] = (canvasWidth - (wallWidth / 2))
             snap = 'r';
         }
         //clear corner vertical
-        if (exitPos[1] > (windowHeight - (4 * wallWidth))) {
-            exitPos[1] = (windowHeight - (4 * wallWidth));
+        if (exitPos[1] > (canvasHeight - (4 * wallWidth))) {
+            exitPos[1] = (canvasHeight - (4 * wallWidth));
         } else if (exitPos[1] < (4 * wallWidth)) {
             exitPos[1] = (4 * wallWidth);
         }
     }
     //console.log(snap);
-    exitPlaced = true;
 }
 
 function signExit() {
     //label exit according to snap direction
     switch (snap) {
         case 't':
-            exitTextPos = exitPos;
-            //exitTextPos[1] = (exitPos[1] + (wallWidth * 4));
+            exitTextPos[0] = (exitPos[0] - (wallWidth * 1.5));
+            exitTextPos[1] = (exitPos[1] + (wallWidth * 4));
             break;
         case 'b':
-            exitTextPos = exitPos;
-            //exitTextPos[1] = (exitPos[1] - wallWidth);
+            exitTextPos[0] = (exitPos[0] - (wallWidth * 1.5));
+            exitTextPos[1] = (exitPos[1] - (wallWidth * 2));
             break;
         case 'l':
-            exitTextPos = exitPos;
-            //exitTextPos[0] = (exitPos[0] + (wallWidth * 4));
-            //exitTextPos[1] = (exitPos[1] + wallWidth);
+            exitTextPos[0] = (exitPos[0] + (wallWidth * 2));
+            exitTextPos[1] = (exitPos[1] + wallWidth);
             break;
         case 'r':
-            exitTextPos = exitPos;
-            //exitTextPos[0] = (exitPos[0] - (wallWidth * 8));
-            //exitTextPos[1] = (exitPos[1] - wallWidth);
+            exitTextPos[0] = (exitPos[0] - (wallWidth * 6));
+            exitTextPos[1] = (exitPos[1] + wallWidth);
             break;
-    }
-    strokeWeight(0);
-    fill(0)
-    text('Exit',exitTextPos[0],exitTextPos[1]);
+        }
     console.log("exitPos: " + exitPos)
     console.log("exitTextPos: " + exitTextPos)
+    exitPlaced = true;
 }
